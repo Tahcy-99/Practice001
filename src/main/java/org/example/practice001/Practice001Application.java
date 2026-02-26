c184348de19d09728ff0ca1cd5854216db5970fa
Tahcy-99 <sjae999@gmail.com>
2026-02-26 14:47:27 +0900

    Initial commit

---
+++ src/main/java/org/example/practice001/Practice001Application.java
@@ -0,0 +1,13 @@
+package org.example.practice001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice001Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/practice001/Practice001ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practice001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
